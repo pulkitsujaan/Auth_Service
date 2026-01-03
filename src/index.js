610f468e5bbe193ff0239ed98b985d69e5f74a7b
pulkitsujaan <pulkitsujaan2708@gmail.com>
2026-01-03 16:44:52 +0530

    First commit: project skeletion prepared

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {PORT} = require('./config/serverConfig');
+const app = express();
+
+const prepareAndStartServer = ()=>{
+
+    app.listen(3001, ()=>{
+        console.log(`Server Started at port: ${PORT}`);
+    })
+}
+
+prepareAndStartServer();
