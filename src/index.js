74ced091eed262f267400bd29e1af6bf9b2cd900
pulkitsujaan <pulkitsujaan2708@gmail.com>
2026-01-03 23:26:26 +0530

    Added getUserById in repository

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 const bodyParser = require('body-parser');
 
-const { User } = require('./models/index');
-const bcrypt = require('bcrypt');
+// const { User } = require('./models/index');
+// const bcrypt = require('bcrypt');
+// const UserRepository = require('./repository/user-repository')
+
+
+
 const app = express();
 
 const prepareAndStartServer = ()=>{
@@ -15,10 +19,16 @@ const prepareAndStartServer = ()=>{
 
     app.listen(3001, async()=>{
         console.log(`Server Started at port: ${PORT}`);
-        const incomingPass = '123456';
-        const user = await User.findByPk(2);
-        const response = bcrypt.compareSync(incomingPass,user.password);
-        console.log(response);
+
+        // const repo = new UserRepository();
+        // const user = await repo.getById(1);
+        // console.log(user);
+
+
+        // const incomingPass = '123456';
+        // const user = await User.findByPk(2);
+        // const response = bcrypt.compareSync(incomingPass,user.password);
+        // console.log(response);
 
     })
 }
