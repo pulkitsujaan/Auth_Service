c01272b654822aa7c3cd4a4c961fbbc4fd83f151
pulkitsujaan <pulkitsujaan2708@gmail.com>
2026-01-05 17:05:02 +0530

    Added validation error class

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -2,25 +2,25 @@ const UserService = require('../services/user-service');
 
 const userService = new UserService();
 
-const create = async(req, res) => {
+const create = async (req, res) => {
     try {
         const result = await userService.create({
-            email:req.body.email,
-            password:req.body.password
+            email: req.body.email,
+            password: req.body.password
         });
         return res.status(201).json({
-            message:'Succesfully created a new user',
-            success:true,
-            data:result,
-            err:{}
-        })
+            message: 'Successfully created a new user',
+            success: true,
+            data: result,
+            err: {}
+        });
     } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: 'Something went wrong',
-            success:false,
-            err: error
-        })
+        return res.status(error.statusCode).json({
+            message: error.message,
+            data: {},
+            success: false,
+            err: error.explanation
+        });
     }
 }
 
